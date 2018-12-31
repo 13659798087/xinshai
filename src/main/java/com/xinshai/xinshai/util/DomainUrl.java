5d3967ec4012fc66b9029a30a668bbf21d170383
13659798087 <1176953999@qq.com>
2018-12-31 08:50:49 +0800

    你好

--- src/main/java/com/xinshai/xinshai/util/DomainUrl.java
+++ src/main/java/com/xinshai/xinshai/util/DomainUrl.java
@@ -9,15 +9,7 @@ public class DomainUrl {
         //natapp生成的域名
         //return "localhost:8070/";
         //return "http://p5fvsu.natappfree.cc/";
-        return "http://wx.lznsn.com/";
-    }
 
-    public static final String appid(){
-        return "wx362203c9bf705039";
-    }
-
-    public static final String appsecret(){
-        return "d316f130b69e3b511a84aca1b50185fa";
     }
 
 }
